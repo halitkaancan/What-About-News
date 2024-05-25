1e99eedb1b5345735bdda066da86d248e6c308f2
Kaan <halitkaancan@gmail.com>
2024-05-26 02:28:30 +0300

    second commit

---
+++ app/src/main/java/com/example/whataboutnews/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.whataboutnews.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "22509acb62ac4d2e816608090ba6bb7b"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500l"
+        const val QUERY_PAGE_SIZE = "20"
+
+    }
+}
